a8b8ac788bb65191b60f44d064d77527bd81e823
Victor Samoylik <v.samojlik@gmail.com>
2022-10-12 16:15:05 +0300

    fix: forget to uncomment  arguments in Dream Team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
+function createDreamTeam(members) {
   if (!Array.isArray(members)) return false;
   let result = [];
   members.forEach((item) => {
